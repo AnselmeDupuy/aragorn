17575b29c64a3c71467a273cdb5d81c3e94bacd9
Mthloow <Mathias.pavie@gmail.com>
2026-02-02 10:14:54 +0100

    BDD

--- src/main/java/com/example/aragorn/model/entity/Equipement.java
+++ src/main/java/com/example/aragorn/model/entity/Equipement.java
@@ -6,28 +6,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import lombok.Data;
 
 @Entity
 @Data
-public class Chevalier {
+public class Equipement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
 
-    @ManyToMany
-    @JoinTable(
-        name = "chevalier_equipements",
-        joinColumns = @JoinColumn(name = "chevalier_id"),
-        inverseJoinColumns = @JoinColumn(
-            name = "equipement_id",
-            unique = true
-        )
-    )
-    private Set<Equipement> equipements;
+    private String type;
+
+    @ManyToMany(mappedBy = "equipements")
+    private Set<Chevalier> chevaliers;
+
 }
