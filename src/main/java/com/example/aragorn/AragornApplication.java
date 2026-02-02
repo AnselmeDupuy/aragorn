dfd3d01f0e9bac71986d2c226e8e58639c491cd3
Anselme <anselmedupuy@gmail.com>
2026-02-02 10:26:18 +0100

    init

---
+++ src/main/java/com/example/aragorn/AragornApplication.java
@@ -0,0 +1,13 @@
+package com.example.aragorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AragornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AragornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aragorn/AragornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aragorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AragornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
