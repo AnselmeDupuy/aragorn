bf2515b2eaeac85809c131e254713ac366e3d9ac
Mthloow <Mathias.pavie@gmail.com>
2026-02-02 10:31:27 +0100

    BDD

--- src/main/java/com/example/aragorn/model/entity/Equipement.java
+++ src/main/java/com/example/aragorn/model/entity/Equipement.java
@@ -6,7 +6,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
@@ -23,4 +25,7 @@ public class Equipement {
     @ManyToMany(mappedBy = "equipements")
     private Set<Chevalier> chevaliers;
 
+    @ManyToOne
+    @JoinColumn(name = "armurerie_id")
+    private Set<Armurerie> armurerie;
 }
