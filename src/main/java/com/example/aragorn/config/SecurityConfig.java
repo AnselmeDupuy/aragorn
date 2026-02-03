c08445e10fa7fb14bf4e757079c1a2dfb22e7e2b
Anselme <anselmedupuy@gmail.com>
2026-02-03 14:19:48 +0100

    removed some addon in tavern

--- src/main/java/com/example/aragorn/config/SecurityConfig.java
+++ src/main/java/com/example/aragorn/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                                            .requestMatchers("/", "/login", "/register", "/css/**", "/js/**", "/error").permitAll()
+                                            .requestMatchers("/", "/login", "/register", "/css/**", "/js/**", "/error", "/taverne").permitAll()
                                             .requestMatchers("/home").authenticated()
                                             .requestMatchers("/champ-de-bataille").hasRole("CHEVALIER")
                                             .requestMatchers("/armurerie").hasRole("CHEVALIER")
